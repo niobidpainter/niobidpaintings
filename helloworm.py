7117bf5d85bf0a010fc65a9c8d079c1156d369b6
niobidpainter <vvvrmillion@gmail.com>
2024-09-04 14:15:40 +1200

    Effectus helloworm.py
    Created a file to mess around and figure out how to use pythonnn. And hopefully figuring out how to use GitHub at the same time.

---
+++ helloworm.py
@@ -0,0 +1,10 @@
+"""
+You can add a multiline string
+not assigned to any variable
+literal, present, ignored
+"""
+import sys
+print(sys.version)
+
+#hello worm! any last squirms?
+print("hellow worm")
