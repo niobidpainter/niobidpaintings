8d63c871a2a6f9a2b85e3478e08e0d9b954bb678
niobidpainter <vvvrmillion@gmail.com>
2024-09-04 15:05:24 +1200

    Renovatio hello.py
    Imagine learning something in Y10 compsci only to forget it years later. Now, imagine learning the EXACT SAME THINGS in Y8. Primary school. Imagine being more proficient in Python at 12 than 17. Imagine, guys.

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-input("What's your name? ")
-print("hello, world!")
+#asking user for name, welcoming
+name = input("What's your name? ")
+print("Hello,", name + "! I am a Python script.", end=" ")
+print("I am text on the same line.")
+# adding the argument end = " " overrides the default end "\n" which would create a new line,
+# now ending with a space and placing the next print function next to the prev text.
